5a9a6464ed4a73ec5259bc0511a22f4dd6dfb35a
Haoyu Lu <110273568+looyulhy@users.noreply.github.com>
2026-06-11 00:48:10 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file with direct plugin declarations
 buildscript {
     val agpVersion = "8.7.3"
     val kotlinVersion = "2.1.0"
@@ -10,12 +9,5 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:$agpVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
